78d1295b06900b97ccb076514f3ed29f1cbba19c
Alexander Shipunov <alexander.shipunov@greenbird.com>
2023-05-11 17:58:45 +0200

    TEMP: some proposals on how to analyse pictures

--- src/main/kotlin/tech/onsibey/squarelife/detector/imageprocessor/Image.kt
+++ src/main/kotlin/tech/onsibey/squarelife/detector/imageprocessor/Image.kt
@@ -4,12 +4,15 @@ import ij.IJ
 import ij.ImagePlus
 import ij.measure.ResultsTable
 import ij.process.ByteProcessor
+import ij.process.ImageProcessor.MIN
+import java.awt.Rectangle
 import java.awt.image.BufferedImage
 import java.awt.image.ColorConvertOp
 import java.io.File
 
 class Processor(private val pathToPhoto: String) {
-    fun processImageBoard(): ImageBoard = ImageBoard(convertToCells(analyzeGrid(File(pathToPhoto))))
+    // fun processImageBoard(): ImageBoard = ImageBoard(convertToCells(analyzeGrid(File(pathToPhoto))))
+    fun processImageBoard(): ImageBoard = ImageBoard(convertToCells(analyzeGridAlternative(File(pathToPhoto))))
 
     private fun convertToCells(analyzedGrid: List<List<Boolean>>): List<List<Cell>> {
         val cells = mutableListOf<MutableList<Cell>>()
@@ -63,6 +66,80 @@ class Processor(private val pathToPhoto: String) {
 
         // Return the resulting 2D list of booleans
         return grid
+    }
+
+    // Proposal on how to analyze the picture
+    private fun analyzeGridAlternative(file: File): List<List<Boolean>> {
+        val imagePlus = IJ.openImage(file.absolutePath)
+        //val originalImageProcessor = imagePlus.processor
+        val imageProcessor = imagePlus.processor
+        imageProcessor.smooth() // Smooth the image to remove noise
+
+        // Color threshold to convert the image to black and white
+        val threshold = 128
+
+        val imageWidth = imageProcessor.width
+        val imageHeight = imageProcessor.height
+
+        // Crop the original image to the size of the grid image
+        // 1. Find the boundaries of the grid image
+        var top: Int = -100
+        var bottom: Int = -100
+        var left: Int = -100
+        var right: Int = -100
+        for (y in 0 until imageHeight) {
+            for (x in 0 until imageWidth) {
+                if (imageProcessor.getValue(x, y) < threshold) {
+                    if (top == -100) top = y
+                    if (top > y) top = y
+
+                    if (bottom == -100) bottom = y
+                    if (bottom < y) bottom = y
+
+                    if (left == -100) left = x
+                    if (left > x) left = x
+
+                    if (right == -100) right = x
+                    if (right < x) right = x
+                }
+            }
+        }
+        // 2. Find width and height of the grid image
+        val gridWidth = right - left
+        val gridHeight = bottom - top
+
+        imageProcessor.roi = Rectangle(left, top, gridWidth, gridHeight)
+        val croppedImageProcessor = imageProcessor.crop()
+        // Trying to smooth the color of the image
+        croppedImageProcessor.filter(MIN)
+        repeat(30) {
+            croppedImageProcessor.smooth()
+        }
+
+        // Uncomment to save and take a look at the cropped picture
+        // ImageIO.write(croppedImageProcessor.bufferedImage, "jpg", File("cropped-preprocessed.jpg"))
+
+        // Convert the cropped image to a 2D list of booleans based on the color threshold
+        val croppedImageWidth = croppedImageProcessor.width
+        val croppedImageHeight = croppedImageProcessor.height
+
+        val monochromeImageRepresentation = Array(croppedImageHeight) { Array(croppedImageWidth) { false } }
+
+        for (y in 0 until croppedImageHeight) {
+            for (x in 0 until croppedImageWidth) {
+                val pixelColor = croppedImageProcessor.getValue(x, y)
+                monochromeImageRepresentation[y][x] = pixelColor > threshold
+            }
+        }
+
+        // Algorithm or conversion of the cropped image to a 2D list of booleans
+        // Find the size of the smallest cell
+        // Use the size of the smallest cell to "split" the image into cells
+        // Check center of each cell to see if it is black or white
+        // Build a 2D list of booleans based on the color of the center of each cell
+
+        // Now we just return the list of booleans based on each pixel of the cropped image [TEMPORARY, NEEDS TO BE REPLACED]
+        return monochromeImageRepresentation.map { it.toList() }.toList()
     }
 
 

92d3b8ac227ef604c2ba8d340d95f63f9ff5dd46
GrindelfP <grindelf.perlomutrovij@gmail.com>
2023-05-13 19:47:58 +0300

    add test for recognizing dominant colour

--- src/test/kotlin/tech/onsibey/squarelife/detector/imageprocessor/ProcessorTest.kt
+++ src/test/kotlin/tech/onsibey/squarelife/detector/imageprocessor/ProcessorTest.kt
@@ -1,5 +1,7 @@
 package tech.onsibey.squarelife.detector.imageprocessor
 
+import ij.ImagePlus
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.io.File
 
@@ -16,11 +18,35 @@ internal class ProcessorTest {
         val stringBuilder = StringBuilder()
         imageBoard.cells.forEach { row ->
             row.forEach { cell ->
-                stringBuilder.append(if (cell.isPainted) "░" else  "█")
+                stringBuilder.append(if (cell.isPainted) "░" else "█")
             }
             stringBuilder.append("\n")
         }
 
         File("testos.txt").writeText(stringBuilder.toString())
+    }
+
+
+    @Test
+    fun `GIVEN mostly black images WHEN dominant colours are estimated THEN black colours is returned`() {
+        val paths = listOf(
+            "/Users/grindelf/Programming/Onsibey/SquareLife/cells/3, 6.jpg",
+            "/Users/grindelf/Programming/Onsibey/SquareLife/cells/3, 4.jpg",
+            "/Users/grindelf/Programming/Onsibey/SquareLife/cells/3, 8.jpg",
+            "/Users/grindelf/Programming/Onsibey/SquareLife/cells/4, 4.jpg"
+        )
+        val images = listOf(
+            ImagePlus(paths[0]),
+            ImagePlus(paths[1]),
+            ImagePlus(paths[2]),
+            ImagePlus(paths[3])
+        )
+        val dominantColours = images.map { image ->
+            image.dominantColour()
+        }
+
+        dominantColours.forEach { colour ->
+            assertThat(colour).isEqualTo(CellColor.BLACK)
+        }
     }
 }

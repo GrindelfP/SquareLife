ad2c36fd537fed8205d9e5cc3a01caa634b7878f
GrindelfP <grindelf.perlomutrovij@gmail.com>
2023-05-13 22:54:08 +0300

    create BoardRecognizer

---
+++ src/main/kotlin/tech/onsibey/squarelife/detector/imageprocessor/BoardRecognizer.kt
@@ -0,0 +1,92 @@
+package tech.onsibey.squarelife.detector.imageprocessor
+
+import ij.ImagePlus
+import kotlin.math.roundToInt
+
+object BoardRecognizer {
+    fun getBoardParameters(imagePlus: ImagePlus, cellParameters: CellParameters): BoardParameters {
+        var numberOfRows = 0
+        var numberOfColumns = 0
+
+        // here we initiate the list of dividers, which is dedicated to determine
+        // the places of checking of the number of rows. As far as the smallest possible
+        // board is 5x5 cells, the dividers will be placed in the expected middle of
+        // the first, third and fifth cell. The horisontal dividers are initiated
+        // with the same expectancies.
+        val verticalDividers = mutableListOf(
+            (cellParameters.width / 2),
+            cellParameters.width / 2 + 2 * cellParameters.width,
+            cellParameters.width / 2 + 4 * cellParameters.width
+        )
+
+        val horizontalDividers = mutableListOf(
+            cellParameters.height / 2,
+            cellParameters.height / 2 + 2 * cellParameters.height,
+            cellParameters.height / 2 + 4 * cellParameters.height
+        )
+
+        for (x in verticalDividers) {
+            var periodCounter = 0
+            for (y in 0 until imagePlus.processor.height) {
+                if (imagePlus.getColor(x, y) != Color.WHITE && periodCounter != 0) {
+                    periodCounter = 0
+                    numberOfRows++
+                }
+                if (imagePlus.getColor(x, y) == Color.WHITE) periodCounter++
+            }
+        }
+
+        for (y in horizontalDividers) {
+            var periodCounter = 0
+            for (x in 0 until imagePlus.processor.width) {
+                if (imagePlus.getColor(x, y) != Color.WHITE) {
+                    periodCounter = 0
+                    numberOfColumns++
+                }
+                if (imagePlus.getColor(x, y) == Color.WHITE) periodCounter++
+            }
+        }
+
+        return BoardParameters(numberOfRows, numberOfColumns)
+    }
+
+    fun getCellParameters(imagePlus: ImagePlus): CellParameters {
+        val setOfWidths = mutableSetOf<Int>()
+        for (x in 0 until imagePlus.processor.width) {
+            var periodCounter = 0
+            for (y in 0 until imagePlus.processor.height) {
+                if (imagePlus.getColor(x, y) != Color.WHITE) {
+                    periodCounter = 0
+                    setOfWidths.add(periodCounter)
+                }
+                if (imagePlus.getColor(x, y) == Color.WHITE) periodCounter++
+            }
+        }
+
+        val setOfHeights = mutableSetOf<Int>()
+        for (y in 0 until imagePlus.processor.height) {
+            var periodCounter = 0
+            for (x in 0 until imagePlus.processor.width) {
+                if (imagePlus.getColor(x, y) != Color.WHITE) {
+                    periodCounter = 0
+                    setOfHeights.add(periodCounter)
+                }
+                if (imagePlus.getColor(x, y) == Color.WHITE) periodCounter++
+            }
+        }
+
+        val paretoWidths = paretoSet(setOfWidths, imagePlus.processor.width)
+        val paretoHeights = paretoSet(setOfHeights, imagePlus.processor.height)
+
+        return CellParameters(paretoWidths.average().roundToInt(), paretoHeights.average().roundToInt())
+    }
+
+    private fun paretoSet(initialSet: MutableSet<Int>, dimension: Int): Set<Int> {
+        val setWithoutDeviations = initialSet.filter { element -> element >= dimension * 0.9 }
+        TODO()
+    }
+}
+
+data class CellParameters(val width: Int, val height: Int)
+
+data class BoardParameters(val numberOfRows: Int, val numberOfColumns: Int)

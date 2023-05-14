b016f7a36c9aa8701df87b49bd94a04456206dfe
GrindelfP <grindelf.perlomutrovij@gmail.com>
2023-05-14 17:53:27 +0300

    update getCellParameters, create tests

---
+++ src/test/kotlin/tech/onsibey/squarelife/detector/imageprocessor/BoardRecognizerTest.kt
@@ -0,0 +1,54 @@
+package tech.onsibey.squarelife.detector.imageprocessor
+
+import ij.ImagePlus
+import org.assertj.core.api.Assertions.assertThat
+import tech.onsibey.squarelife.detector.imageprocessor.BoardRecognizer.getCellParameters
+import kotlin.test.Test
+
+class BoardRecognizerTest {
+    @Test
+    fun `GIVEN empty photo 200x200 WHEN applying getCellParameters() THEN returns cell size 200x200`() {
+        val imageWithWrapper = ImagePlus("/Users/grindelf/Programming/Onsibey/SquareLife/photos/с0.jpeg")
+        val image = imageWithWrapper.processor
+        val cellParameters = getCellParameters(image)
+
+        assertThat(cellParameters.height).isEqualTo(200)
+        assertThat(cellParameters.width).isEqualTo(200)
+    }
+
+    @Test
+    fun `GIVEN photo with 12 cells 200x200 WHEN applying getCellParameters() THEN returns cell size is between 67x50 and 50x40`() {
+        val imageWithWrapper = ImagePlus("/Users/grindelf/Programming/Onsibey/SquareLife/photos/с12_2.jpeg")
+        val image = imageWithWrapper.processor
+        val cellParameters = getCellParameters(image)
+
+        println(cellParameters)
+
+        assertThat(cellParameters.width).isBetween(50, 67)
+        assertThat(cellParameters.height).isBetween(40, 50)
+    }
+
+    @Test
+    fun `GIVEN photo with 4 cells 200x200 WHEN applying getCellParameters() THEN returns cell size is between 100x100 and 50x50`() {
+        val imageWithWrapper = ImagePlus("/Users/grindelf/Programming/Onsibey/SquareLife/photos/с4.jpeg")
+        val image = imageWithWrapper.processor
+        val cellParameters = getCellParameters(image)
+
+        println(cellParameters)
+
+        assertThat(cellParameters.width).isBetween(50, 100)
+        assertThat(cellParameters.height).isBetween(50, 100)
+    }
+
+    @Test
+    fun `GIVEN image of board with 100 cells WHEN applying getCellParameters() THEN returns cell size is between 65x75 and 72x83`() {
+        val imageWithWrapper = ImagePlus("/Users/grindelf/Programming/Onsibey/SquareLife/photos/t1.jpeg")
+        val image = imageWithWrapper.processor
+        val cellParameters = getCellParameters(image)
+
+        println(cellParameters)
+
+        assertThat(cellParameters.width).isBetween(65, 72)
+        assertThat(cellParameters.height).isBetween(75, 83)
+    }
+}

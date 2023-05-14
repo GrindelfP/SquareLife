d67b3afd9afa52264262f6a0841193e7bc066810
GrindelfP <grindelf.perlomutrovij@gmail.com>
2023-05-14 17:53:27 +0300

    update getCellParameters, create tests

--- src/main/kotlin/tech/onsibey/squarelife/detector/imageprocessor/BoardRecognizer.kt
+++ src/main/kotlin/tech/onsibey/squarelife/detector/imageprocessor/BoardRecognizer.kt
@@ -33,35 +33,72 @@ object BoardRecognizer {
     }
 
     fun getCellParameters(imageProcessor: ImageProcessor): CellParameters {
-        val setOfWidths = mutableSetOf<Int>()
-        for (x in 0 until imageProcessor.width) {
-            var periodCounter = 0
-            for (y in 0 until imageProcessor.height) {
-                if (imageProcessor.getColor(x, y) != Color.WHITE) {
-                    periodCounter = 0
-                    setOfWidths.add(periodCounter)
-                }
-                if (imageProcessor.getColor(x, y) == Color.WHITE) periodCounter++
+        val imageWidth = imageProcessor.width
+        val imageHeight = imageProcessor.height
+
+        val setOfWidths = getWhiteLines(Direction.HORIZONTAL, imageWidth, imageHeight, imageProcessor)
+        val setOfHeights = getWhiteLines(Direction.VERTICAL, imageWidth, imageHeight, imageProcessor)
+
+        if (setOfWidths.isEmpty() || setOfHeights.isEmpty()) throw Exception("No cells found!")
+
+        return when {
+            setOfWidths.size == 1 && setOfHeights.size == 1 -> {
+                CellParameters(setOfWidths.first(), setOfHeights.first())
+            }
+            else -> {
+                val averageWidth = SingleCriteriaParetoSet(setOfWidths.toList()).averageInt()
+                val averageHeights = SingleCriteriaParetoSet(setOfHeights.toList()).averageInt()
+                CellParameters(averageWidth, averageHeights)
             }
         }
-
-        val setOfHeights = mutableSetOf<Int>()
-        for (y in 0 until imageProcessor.height) {
-            var periodCounter = 0
-            for (x in 0 until imageProcessor.width) {
-                if (imageProcessor.getColor(x, y) != Color.WHITE) {
-                    periodCounter = 0
-                    setOfHeights.add(periodCounter)
-                }
-                if (imageProcessor.getColor(x, y) == Color.WHITE) periodCounter++
-            }
-        }
-
-        val averageWidth = SingleCriteriaParetoSet(setOfWidths.toList()).averageInt()
-        val averageHeights = SingleCriteriaParetoSet(setOfHeights.toList()).averageInt()
-
-        return CellParameters(averageWidth, averageHeights)
     }
+
+    private fun getWhiteLines(direction: Direction, width: Int, height: Int, imageProcessor: ImageProcessor) : Set<Int> {
+        val whiteLines = mutableSetOf<Int>()
+
+        val widthLimit = when (direction) {
+            Direction.HORIZONTAL -> width
+            Direction.VERTICAL -> height
+        }
+
+        val heightLimit = when (direction) {
+            Direction.HORIZONTAL -> height
+            Direction.VERTICAL -> width
+        }
+
+        for (y in 0 until heightLimit) {
+            var periodCounter = 0
+            for (x in 0 until widthLimit) {
+
+                val xToCheck = when (direction) {
+                    Direction.HORIZONTAL -> x
+                    Direction.VERTICAL -> y
+                }
+                val yToCheck = when (direction) {
+                    Direction.HORIZONTAL -> y
+                    Direction.VERTICAL -> x
+                }
+
+                if (imageProcessor.getColor(xToCheck, yToCheck) != Color.WHITE && periodCounter != 0) {
+                    periodCounter = 0
+                    whiteLines.add(periodCounter)
+                }
+                else if (x == imageProcessor.height - 1) {
+                    whiteLines.add(periodCounter + 1)
+                    periodCounter = 0
+                }
+                else if (imageProcessor.getColor(xToCheck, yToCheck) == Color.WHITE) periodCounter++
+            }
+        }
+
+        return whiteLines
+    }
+}
+
+data class Coordinate(val x: Int, val y: Int)
+
+enum class Direction {
+    HORIZONTAL, VERTICAL
 }
 
 data class CellParameters(val width: Int, val height: Int)
